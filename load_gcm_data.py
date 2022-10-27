b34eb3bdfbfd242d0434f51210ff34c9a5b3c615
Simo Siiriä <simo.siiria@fmi.fi>
2022-10-27 11:22:08 +0300

    base version of loading into netcdf works

--- load_gcm_data.py
+++ load_gcm_data.py
@@ -5,8 +5,8 @@ import xarray as xr
 
 oper_dir = "/mnt/c/Data/alga_test/"
 product_name = "fmi::forecast::harmonie::surface::grid"
-#product_args = ["bbox=16,59,30,66"]
-product_args = ["bbox=18,59,20,60"]
+product_args = ["bbox=16,59,30,66"]
+#product_args = ["bbox=18,59,20,60"]
 variables = ['Mean sea level pressure', 'Surface net solar radiation']
 grid_type = 'latlon'    # latlon means we can store just first lat-lon rows.
                         # Anything else means the whole grid must be saved
@@ -30,16 +30,17 @@ else:
 gathered_data['time'] = (('time',), time_axis)
 
 for var in variables:
+    the_unit = last_data.data[min(time_axis)][min(layers)][var]['units']
     print("{} ({}), size = {}".format(var, \
-            last_data.data[min(time_axis)][min(layers)][var]['units'], \
+            the_unit, \
             last_data.data[min(time_axis)][min(layers)][var]['data'].shape))
     new_var = []
     for time in time_axis:
         data = last_data.data[time]
         data = data[min(data.keys())] # take the lowest layer
-        new_var.append(xr.DataArray(data[var]['data']))
+        new_var.append(xr.DataArray(data[var]['data'], dims = ['lat','lon'], attrs = {'units':the_unit}))
 #    gathered_data[var] = (('time','lat','lon'),xr.concat(new_var, dim = time_axis))
-    gathered_data[var] = xr.concat(new_var, dim = time_axis)
+    gathered_data[var] = xr.concat(new_var, dim = xr.DataArray(time_axis, dims = 'time'))
 
 
 data_xr = xr.Dataset(gathered_data)
