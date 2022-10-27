af9962a0b4ac1cb66048c1fe991f24de82e0c819
Simo Siiriä <simo.siiria@fmi.fi>
2022-10-27 13:39:28 +0300

    base fetching done

--- load_gcm_data.py
+++ load_gcm_data.py
@@ -1,20 +1,35 @@
 import fmiopendata
 from fmiopendata.wfs import download_stored_query as fmi_dsq
 import xarray as xr
-
+import time
 
 oper_dir = "/mnt/c/Data/alga_test/"
 product_name = "fmi::forecast::harmonie::surface::grid"
+"""
+List of available products: https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=describeStoredQueries
+
+Products that might be useful:
+    fmi::forecast::harmonie::surface::grid
+    ecmwf::forecast::surface::grid
+    fmi::forecast::hbm::grid
+    fmi::forecast::wam::grid
+
+"""
 product_args = ["bbox=16,59,30,66"]
 #product_args = ["bbox=18,59,20,60"]
-variables = ['Mean sea level pressure', 'Surface net solar radiation']
+parameters = [  'Time-integrated surface direct short wave radiation', \
+                'Surface net solar radiation', \
+                ]
+
 grid_type = 'latlon'    # latlon means we can store just first lat-lon rows.
                         # Anything else means the whole grid must be saved
 print('fetching metadata...')
 model_data = fmi_dsq(product_name, args = product_args)
 last_data = model_data.data[max(model_data.data.keys())]
+time0 = time.time()
 print('fetching data...')
 last_data.parse(delete = True)
+load_time = time.time() - time0 #seconds
 
 gathered_data = {}
 latitudes = last_data.latitudes
@@ -29,9 +44,15 @@ else:
     gathered_data['longitude'] = (('lat','lon'), longitudes)
 gathered_data['time'] = (('time',), time_axis)
 
-for var in variables:
+print("Model data {} acquired successfully. ({:.1f} seconds)".format(product_name, load_time))
+print("Available parameters:")
+for v in last_data.data[min(time_axis)][min(layers)].keys():
+    print("{} ({})".format(v, last_data.data[min(time_axis)][min(layers)][v]['units']))
+print()
+
+for var in parameters:
     the_unit = last_data.data[min(time_axis)][min(layers)][var]['units']
-    print("{} ({}), size = {}".format(var, \
+    print("Processing:  ({}), size = {}".format(var, \
             the_unit, \
             last_data.data[min(time_axis)][min(layers)][var]['data'].shape))
     new_var = []
@@ -44,4 +65,5 @@ for var in variables:
 
 
 data_xr = xr.Dataset(gathered_data)
-data_xr.to_netcdf(oper_dir + "meteor_data.nc")
+data_xr.to_netcdf(oper_dir + "meteor_data.nc", 'w')
+print("Saved: {}".format(oper_dir + "meteor_data.nc"))
