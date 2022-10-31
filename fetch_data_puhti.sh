e22554c2d6eb94c59a96cadf6baeb4169ab28122
Simo-Matti Siiriä <simo.siiria@fmi.fi>
2022-10-31 11:04:51 +0200

    added first version of marine data loading

--- fetch_data_puhti.sh
+++ fetch_data_puhti.sh
@@ -4,4 +4,5 @@ module load tykky
 export PATH="/projappl/project_2001981/siiriasi/AGEnv/bin/:$PATH"
 cd /projappl/project_2001981/siiriasi/algae_proj/AlgaProj
 python load_athmospheric_forcing.py
+python load_marine_forcing.py
 
