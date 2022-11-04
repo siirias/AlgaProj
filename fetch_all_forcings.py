39fafd179ea41f604934341928160d62b805205b
Simo Siiriä <simo.siiria@fmi.fi>
2022-11-04 15:36:32 +0200

    split fetch_data_puhti in two, so inner part can be used outside puhti

---
+++ fetch_all_forcings.py
@@ -0,0 +1,11 @@
+"""
+Simple script to just run all the parts of the suite
+which gather data from various sources
+"""
+import os
+
+os.system('python load_athmospheric_forcing.py')
+os.system('python load_hydrodyn_forcing.py')
+os.system('python load_wave_forcing.py')
+os.system('python load_bgc_forcing.py')
+os.system('python load_ship_data.py')
--- fetch_data_puhti.sh
+++ fetch_data_puhti.sh
@@ -3,8 +3,5 @@ module purge
 module load tykky
 export PATH="/projappl/project_2001981/siiriasi/AGEnv/bin/:$PATH"
 cd /projappl/project_2001981/siiriasi/algae_proj/
-python load_athmospheric_forcing.py
-python load_hydrodyn_forcing.py
-python load_wave_forcing.py
-python load_bgc_forcing.py
+python fecth_all_forcings.py
 
